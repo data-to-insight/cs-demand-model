97b0b9dda18899832e7534afeb7d97edd626f048
tab1tha <tambetachere@gmail.com>
2022-04-20 15:28:50 +0100

    create subkey before assignment

--- csdmpy/super_model.py
+++ csdmpy/super_model.py
@@ -69,6 +69,7 @@ def transition_probs_per_bracket(df, bin_defs, start_date, end_date):
 def daily_entrants_per_bracket(df, bin_defs, start_date, end_date):
     entrants_mat = {}
     for age_bin in bin_defs:
+        entrants_mat[age_bin] = {}
         placement_types = bin_defs[age_bin]
         if len(placement_types) == 1:
             entrants_mat[age_bin] = pd.DataFrame(data=1.0, index=placement_types, columns=placement_types)
