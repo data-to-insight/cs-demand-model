cd1ced66135a6d28660a6814a780881487c2486b
tab1tha <tambetachere@gmail.com>
2022-05-09 12:57:56 +0100

    test_step_to_days works

--- csdmpy/tests/test_super_model.py
+++ csdmpy/tests/test_super_model.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from csdmpy.super_model import get_daily_entrants, get_daily_transition_rates
+from csdmpy.super_model import get_daily_entrants, get_daily_transition_rates, step_to_days
 
 def test_get_daily_entrants(dummy_entrant_eps):
     cat_list = ('Foster', 'Resi', 'Supported', 'Other')
@@ -22,3 +22,10 @@ def test_get_daily_transition_rates(dummy_entrant_eps):
     # check values
     assert transition_rates.loc['Resi', 'Foster'].round(decimals=3) == 0.028
     assert transition_rates.loc['Foster', 'Resi'].round(decimals=3) == 0.083
+
+def test_step_to_days():
+    days = step_to_days(step_size='7m')
+    assert days == 210
+
+    days = step_to_days(step_size='5d')
+    assert days == 5
